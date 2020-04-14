887f45afd497b1723b45abade20f92452ead2cd8
DmitryMI <wet75787@tqosi.com>
2020-04-14 15:27:25 +0300

    Weighted graph bug fixed

--- Software/PathFindingAlgorithms/PathFinders/Graphs/GraphGenerator.cs
+++ Software/PathFindingAlgorithms/PathFinders/Graphs/GraphGenerator.cs
@@ -6,51 +6,6 @@ namespace PathFinders.Graphs
     public static class GraphGenerator
     {
         public static double Sqrt2 = Math.Sqrt(2);
-        
-
-        public static GraphNode[,] GetGraph(ICellMap map, NeighbourMode neighbourMode)
-        {
-            GraphNode[,] nodes = new GraphNode[map.Width,map.Height];
-            for (int x = 0; x < map.Width; x++)
-            {
-                for (int y = 0; y < map.Height; y++)
-                {
-                    if (map.IsPassable(x, y))
-                    {
-                        GraphNode prevNode;
-                        GraphNode currentNode = new GraphNode();
-                        nodes[x, y] = currentNode;
-                        currentNode.Position = new Vector2Int(x, y);
-
-                        int prevX = x - 1;
-                        int prevY = y - 1;
-                        if (prevX >= 0)
-                        {
-                            prevNode = nodes[prevX, y];
-                            ConnectNodes(currentNode, prevNode);
-                        }
-
-                        if (prevY >= 0)
-                        {
-                            prevNode = nodes[x, prevY];
-                            ConnectNodes(currentNode, prevNode);
-                        }
-
-                        if (prevX >= 0 && prevY >= 0 && neighbourMode == NeighbourMode.SidesAndDiagonals)
-                        {
-                            prevNode = nodes[prevX, prevY];
-                            ConnectNodes(currentNode, prevNode);
-                        }
-                    }
-                    else
-                    {
-                        nodes[x, y] = null;
-                    }
-                }
-            }
-
-            return nodes;
-        }
 
         private static void ConnectNodes(GraphNode nodeA, GraphNode nodeB)
         {
@@ -74,6 +29,58 @@ namespace PathFinders.Graphs
             nodeB.SetWeight(nodeA, weight);
         }
 
+
+        public static GraphNode[,] GetGraph(ICellMap map, NeighbourMode neighbourMode)
+        {
+            GraphNode[,] nodes = new GraphNode[map.Width,map.Height];
+            for (int x = 0; x < map.Width; x++)
+            {
+                for (int y = 0; y < map.Height; y++)
+                {
+                    if (map.IsPassable(x, y))
+                    {
+                        GraphNode connectionNode;
+                        GraphNode currentNode = new GraphNode();
+                        nodes[x, y] = currentNode;
+                        currentNode.Position = new Vector2Int(x, y);
+
+                        int prevX = x - 1;
+                        int prevY = y - 1;
+                        int nextX = x + 1;
+
+                        if (nextX < map.Width && prevY >= 0 && neighbourMode == NeighbourMode.SidesAndDiagonals)
+                        {
+                            connectionNode = nodes[nextX, prevY];
+                            ConnectNodes(currentNode, connectionNode);
+                        }
+                        if (prevX >= 0)
+                        {
+                            connectionNode = nodes[prevX, y];
+                            ConnectNodes(currentNode, connectionNode);
+                        }
+
+                        if (prevY >= 0)
+                        {
+                            connectionNode = nodes[x, prevY];
+                            ConnectNodes(currentNode, connectionNode);
+                        }
+
+                        if (prevX >= 0 && prevY >= 0 && neighbourMode == NeighbourMode.SidesAndDiagonals)
+                        {
+                            connectionNode = nodes[prevX, prevY];
+                            ConnectNodes(currentNode, connectionNode);
+                        }
+                    }
+                    else
+                    {
+                        nodes[x, y] = null;
+                    }
+                }
+            }
+
+            return nodes;
+        }
+
         public static WeightedGraph<double> GetWeightedGraph(ICellMap map, NeighbourMode neighbourMode)
         {
             WeightedGraphNode<double>[,] nodes = new WeightedGraphNode<double>[map.Width, map.Height];
@@ -84,28 +91,36 @@ namespace PathFinders.Graphs
                 {
                     if (map.IsPassable(x, y))
                     {
-                        WeightedGraphNode<double> prevNode;
+                        WeightedGraphNode<double> connectionNode;
                         WeightedGraphNode<double> currentNode = new WeightedGraphNode<double>(Double.PositiveInfinity);
                         nodes[x, y] = currentNode;
                         currentNode.Position = new Vector2Int(x, y);
 
                         int prevX = x - 1;
                         int prevY = y - 1;
+                        int nextY = y + 1;
+
                         if (prevX >= 0)
                         {
-                            prevNode = nodes[prevX, y];
-                            ConnectNodes(currentNode, prevNode, 1);
+                            connectionNode = nodes[prevX, y];
+                            ConnectNodes(currentNode, connectionNode, 1);
                         }
                         if (prevY >= 0)
                         {
-                            prevNode = nodes[x, prevY];
-                            ConnectNodes(currentNode, prevNode, 1);
+                            connectionNode = nodes[x, prevY];
+                            ConnectNodes(currentNode, connectionNode, 1);
                         }
 
                         if (prevX >= 0 && prevY >= 0 && neighbourMode == NeighbourMode.SidesAndDiagonals)
                         {
-                            prevNode = nodes[prevX, prevY];
-                            ConnectNodes(currentNode, prevNode, Sqrt2);
+                            connectionNode = nodes[prevX, prevY];
+                            ConnectNodes(currentNode, connectionNode, Sqrt2);
+                        }
+
+                        if (nextY < map.Height && prevX >= 0 && neighbourMode == NeighbourMode.SidesAndDiagonals)
+                        {
+                            connectionNode = nodes[prevX, nextY];
+                            ConnectNodes(currentNode, connectionNode, Sqrt2);
                         }
                     }
                     else
